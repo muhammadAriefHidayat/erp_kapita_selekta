094f5b358353159a0c1f416240693f6a36ee9cb3
muhammadariefhidayat <tubagusarief45@gmail.com>
2018-12-05 14:14:04 +0700

    crud untuk pelanggan pabrik pesawat

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Pabrik Pesawat',
+    'version': '1.0',
+    'category': 'Tools',
+    'author':'Arief dan Rahma',
+    'summary': 'Manajemen Pesawat dan Pelanggan',
+    'description':'Muhammad Arief Hidayat: 1517051086 '
+                  'Noverina Rahmaniyanti: 1517051096',
+    'depends': ['web'],
+    'data': [
+        'data/actions.xml',
+    ],
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
